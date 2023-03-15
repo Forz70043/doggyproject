93c04bb0e332142f053e07fbe0e0f3138d845739
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 19:01:15 +0100

    Clean artist page

--- pages/artists.js
+++ pages/artists.js
@@ -13,53 +13,10 @@ export const getStaticProps = async () => {
 
 const ArtistPage = () => {
   return (
-    
-    <div className={styles.center}>
+    <>
       <h1>Artist Page</h1>
-      <p>Here you can find information about our artists.</p>
-
-
-      <div className={styles.grid}>
-      <a
-        href="/events"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.className}>
-          Events <span>-&gt;</span>
-        </h2>
-        <p className={styles.className}>
-          Find in-depth information about our events.
-        </p>
-      </a>
-
-      <a href="/artists" className={styles.card} target="_blank" rel="noopener noreferrer">
-        <h2 className={styles.className}>
-          Artists <span>-&gt;</span>
-        </h2>
-        <p className={styles.className}>
-          Find in-depth information about our artists!
-        </p>
-      </a>
-
-      <a
-        href="#"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={styles.className}>
-          Posdcast <span>-&gt;</span>
-        </h2>
-        <p className={styles.className}>
-          Listen our Podcast
-        </p>
-      </a>
-      </div>
-
-
-    </div>
+      <p>Here you can find information about our artists.</p> 
+    </>
   )
 }
 
