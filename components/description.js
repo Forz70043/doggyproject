80f002daf7666b7a71e9fa35362f0c5dd22ccd04
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 17:51:06 +0100

    Added description component

---
+++ components/description.js
@@ -0,0 +1,24 @@
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+
+
+export default function Description(){
+    return (
+        <>
+        <div className={styles.description}>
+            <p>Lorem ipsum dolor sit amet,&nbsp;
+                <code className={styles.code}> XXXXXX</code>
+            </p>
+            <div>
+            <a href="" target="_blank" rel="noopener noreferrer">By{' '}
+              <Image src="/AP-dev.png" alt="AP Logo" className={styles.apLogo}
+                width={100}
+                height={90}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+        </>
+    )
+}
