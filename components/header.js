ca80653fc25968609774db7df82c21e040a492f9
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 17:50:14 +0100

    Added header component

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function Header(){
+    return (
+        <Head>
+            <title>Doggy Project</title>
+            <meta name="description" content="Doggy Project" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="description" content="Description" />
+            <meta name="keywords" content="Keywords" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
