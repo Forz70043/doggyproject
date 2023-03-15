0a809bcd47c3a4cf30822089d7a4ad49becbe732
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 17:53:01 +0100

    Added i18n utils

---
+++ utils/i18n.js
@@ -0,0 +1,12 @@
+/*let NextI18Next = require('next-i18next').default
+
+module.exports = new NextI18Next({
+  defaultLanguage: 'en',
+  otherLanguages: ['it', 'es'],
+  localeSubpaths: {
+    en: 'en',
+    es: 'es',
+    it: 'it'
+  }
+})
+*/
