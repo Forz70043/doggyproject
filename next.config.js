6cc59afe75ffc1e8acf54405f0e77fa0d04397a9
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 18:05:14 +0100

    Update nect config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa'
+import { i18n } from './utils/i18n'
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es', 'it'],
+    defaultLocale: 'en',
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
