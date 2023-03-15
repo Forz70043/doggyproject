4bcafd39880f81ffa447f5f89d7b712a633b79e0
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 17:58:56 +0100

    Update app for layout nested

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Layout from '@/components/layout'
+//import { appWithTranslation } from '../utils/i18n'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
