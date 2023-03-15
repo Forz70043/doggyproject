a97ed4ac95327001993f9fd3aa5441898abe9550
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 18:04:17 +0100

    Added events page

---
+++ pages/events.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css'
+
+
+const EventsPage = () => {
+  return (
+    <div className={styles.center}>
+      <h1>Events Page</h1>
+      <p>Here you can find information about our upcoming events.</p>
+    </div>
+  )
+}
+
+export default EventsPage
