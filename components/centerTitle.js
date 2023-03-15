d4cd739af1593c05e3fde89c4ef45d2a04abe502
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-15 17:50:53 +0100

    Added centerTitle component

---
+++ components/centerTitle.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+export default function centerTitle(){
+    return (
+        <>
+        <div className={styles.center}>
+            <h2 id="spaceGrotesk">Doggy Project</h2>
+        </div>
+        </>
+    )
+}
