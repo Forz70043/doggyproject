3848eb9e8fb1dbfc63860d215d67d0f21066b861
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-16 20:51:37 +0100

    Update Artist component

--- components/Artist.js
+++ components/Artist.js
@@ -1,7 +1,23 @@
-import styles from '@/styles/Home.module.css'
+import artistCss from '@/styles/Artist.module.css';
+import Image from 'next/image'
 
 
-
-export default function ArtistComponent(){
-    return (<></>)
+export default function ArtistComponent(props){
+    
+    return (
+        <>
+            <div className={artistCss.flipCard}>
+                <div className={artistCss.flipCardInner}>
+                    <div className={artistCss.flipCardFront}>
+                        <Image src={props.src} alt="Artist Img" width={200} height={200} priority/>
+                    </div>
+                    <div className={artistCss.flipCardBack}>
+                        <h1>{props.aka}</h1>
+                        
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+            </div> 
+        </>
+    )
 }
