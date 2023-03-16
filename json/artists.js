974c93a00a012501b5747c6d58cb193369a6125d
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-16 20:50:05 +0100

    Added src attr to json

--- json/artists.js
+++ json/artists.js
@@ -2,26 +2,31 @@ export let artists = [
     {
         "name": "Cosimo Petrosillo",
         "aka": "Unreal Vibes",
-        "description": "Cosimo Petrosillo, nacido en 1997, alias @unrealvibe.s comenzó a crear sonidos a los 14 años, introduciendo la música electrónica en su día a día. Al cabo de unos años se mudó a Londres por trabajo, y ahí nació su pasión por los vinilos, pasaba días buscando discos en las mejores tiendas de discos de la capital inglesa. Sus sets van del house al techno, con matices break y acid. En 2021 firma su primer lanzamiento con el sello independiente @apuladoggyrecords. Se ha abierto a DJs como Alex Neri, Dj Plant Texure, Marcellus Pittman."
+        "description": "Cosimo Petrosillo, nacido en 1997, alias @unrealvibe.s comenzó a crear sonidos a los 14 años, introduciendo la música electrónica en su día a día. Al cabo de unos años se mudó a Londres por trabajo, y ahí nació su pasión por los vinilos, pasaba días buscando discos en las mejores tiendas de discos de la capital inglesa. Sus sets van del house al techno, con matices break y acid. En 2021 firma su primer lanzamiento con el sello independiente @apuladoggyrecords. Se ha abierto a DJs como Alex Neri, Dj Plant Texure, Marcellus Pittman.",
+        "src":'/unrealvibes.png'
     },
     {
         "name": "Alberto Fiume",
         "aka": "Alberto River",
-        "description": "Alberto Fiume, alias @alberto_river_dj , nació en Monopoli de la clase 93, entró en el mundo de la producción en 2013, donde creó sus primeros trabajos con el sello orientalbeatz. Su estilo incluye Afro House con matices tribales."
+        "description": "Alberto Fiume, alias @alberto_river_dj , nació en Monopoli de la clase 93, entró en el mundo de la producción en 2013, donde creó sus primeros trabajos con el sello orientalbeatz. Su estilo incluye Afro House con matices tribales.",
+        "src":'/albertoriver.png'
     },
     {
         "name": "Nicholas La bella",
         "aka": "BBBROTH3R",
-        "description": "Nicholas La bella, aka @bbbroth3r.dj Nacido en Roma, buscando de inmediato su lugar en el mundo de la música, Bbbroth3r se dio a conocer con su primer EP Spider Gin, la síntesis perfecta de su visión musical. Tiene 26 años y sus ideas parecen bastante claras. Compuso el segundo tema titulado 68 y otros 4 caracterizados por un estilo esencial y una potencia y calidad que trastorna las reglas."
+        "description": "Nicholas La bella, aka @bbbroth3r.dj Nacido en Roma, buscando de inmediato su lugar en el mundo de la música, Bbbroth3r se dio a conocer con su primer EP Spider Gin, la síntesis perfecta de su visión musical. Tiene 26 años y sus ideas parecen bastante claras. Compuso el segundo tema titulado 68 y otros 4 caracterizados por un estilo esencial y una potencia y calidad que trastorna las reglas.",
+        "src":'/dj3.png'
     },
     {
         "name": "Valerio Parrotta",
         "aka": "BBBROTH3R",
-        "description": "A temprana edad comenzó a estudiar piano, lo que sentó las bases de su camino musical. Con el tiempo, se acerca al mundo de la música electrónica, particu-larmente al género ambient/ambient techno.Todo esto ha caracterizado su particular sonido que alterna sonidos de instrumentos clásicos con sinteti-zadores y ritmos analógicos.Comenzó sus estudios en la SAE de Milán, licencián-dose en Producción de Música Electrónica e Ingeniero de Sonido.En 2019 se mudó a Berlín, donde comenzó sus estudios en dBs Music en Berlín, con una maestría en Diseño y Interpretación de Sonido.A lo largo de los años actuará en actuaciones en directo y DJ sets participando en diversos festivales locales y también en el extranjero (América, Francia, Suiza, Holanda (ADE) y Alemania) compartiendo escenario con artistas como Rival Consoles, Indian Wells, Mathew Jonson, Parallx, Romare, Nick Williams, Tama Sumo, Lakuti y muchos otros."
+        "description": "A temprana edad comenzó a estudiar piano, lo que sentó las bases de su camino musical. Con el tiempo, se acerca al mundo de la música electrónica, particu-larmente al género ambient/ambient techno.Todo esto ha caracterizado su particular sonido que alterna sonidos de instrumentos clásicos con sinteti-zadores y ritmos analógicos.Comenzó sus estudios en la SAE de Milán, licencián-dose en Producción de Música Electrónica e Ingeniero de Sonido.En 2019 se mudó a Berlín, donde comenzó sus estudios en dBs Music en Berlín, con una maestría en Diseño y Interpretación de Sonido.A lo largo de los años actuará en actuaciones en directo y DJ sets participando en diversos festivales locales y también en el extranjero (América, Francia, Suiza, Holanda (ADE) y Alemania) compartiendo escenario con artistas como Rival Consoles, Indian Wells, Mathew Jonson, Parallx, Romare, Nick Williams, Tama Sumo, Lakuti y muchos otros.",
+        "src":'/valerioperrotta.png'
     },
     {
-        "name": "Valerio Parrotta",
-        "aka": "BBBROTH3R",
-        "description": "A temprana edad comenzó a estudiar piano, lo que sentó las bases de su camino musical. Con el tiempo, se acerca al mundo de la música electrónica, particu-larmente al género ambient/ambient techno.Todo esto ha caracterizado su particular sonido que alterna sonidos de instrumentos clásicos con sinteti-zadores y ritmos analógicos.Comenzó sus estudios en la SAE de Milán, licencián-dose en Producción de Música Electrónica e Ingeniero de Sonido.En 2019 se mudó a Berlín, donde comenzó sus estudios en dBs Music en Berlín, con una maestría en Diseño y Interpretación de Sonido.A lo largo de los años actuará en actuaciones en directo y DJ sets participando en diversos festivales locales y también en el extranjero (América, Francia, Suiza, Holanda (ADE) y Alemania) compartiendo escenario con artistas como Rival Consoles, Indian Wells, Mathew Jonson, Parallx, Romare, Nick Williams, Tama Sumo, Lakuti y muchos otros."
+        "name": "Cosimo Satalino",
+        "aka": "Konsat",
+        "description": "Konsat es un DJ italiano afincado en Monopoli, su ciudad natal. Desde que era un niño ha estado fascinado por los clubes y discotecas. Ha decidido crecer con la música electrónica desde 2017 cuando compró su primer equipo. Siempre inspirado por la música underground como el hard techno, trance, industrial, american deep y house de los 90. Trabaja en algunos clubs del sur de Italia y ha colaborado con artistas internacionales como Mattia Trani, Parallax y otros.Recientemente jugó en @theclubmlg junto con Vael.",
+        "src":'/cosimosatalino.png'
     }
 ]
