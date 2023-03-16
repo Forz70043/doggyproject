0ebc4bd2da1248107d350b6656c9dbcbac9ab498
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-16 19:58:48 +0100

    Update certerTitle component

--- components/CenterTitle.js
+++ components/CenterTitle.js
@@ -1,10 +1,12 @@
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
 
 export default function centerTitle(props){
+    //<h2 id="spaceGrotesk">{props.title ?? ''}</h2>
     return (
         <>
         <div className={styles.center}>
-            <h2 id="spaceGrotesk">{props.title ?? ''}</h2>
+            <Image src="/logo_empty.png" alt="Doggy Logo" className={styles.thirteen} width={400} height={400} />
         </div>
         </>
     )
