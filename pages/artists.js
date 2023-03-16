d782dd7e9f63e91ef4376faa0287eed6372f897c
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-16 20:52:03 +0100

    Update artist page

--- pages/artists.js
+++ pages/artists.js
@@ -1,10 +1,8 @@
 let artists = require('json/data.json');
 import React from 'react'
-
+import artistCss from '@/styles/Artist.module.css';
 import CenterTitle from '@/components/CenterTitle';
-import GridElement from '@/components/GridElement';
-
-console.log(artists);
+import ArtistComponent from '@/components/Artist';
 
 export const getStaticProps = async () => {
   return {
@@ -15,15 +13,17 @@ export const getStaticProps = async () => {
 }
 
 const ArtistPage = (props) => {
-  console.log(props)
   
   return (
     <>
       <CenterTitle title="Artists"/>
       <p>Here you can find information about our artists.</p>
-      {props.artists.map(elem => {
-        return <GridElement title={elem.aka}  />
-      })}
+      
+      <div className={artistCss.gridArtists} >
+        {props.artists.map((elem, index) => {
+          return <ArtistComponent key={index} aka={elem.aka} description={elem.description} src={elem.src}/>
+        })}
+      </div>
       
     </>
   )
