96c1ddb29935dcc5827412f404e0ddf4fc5023c8
Dantesk <lagalante.p@gmail.com>
2023-03-18 20:14:45 +0100

    integrate svg and header title

--- components/HeaderEvents.js
+++ components/HeaderEvents.js
@@ -1,6 +1,6 @@
 
 
-export default function HeaderArtist() {
+export default function HeaderEvents() {
 
     return (
         <div className="bg-white dark:bg-slate-800 pt-24 px-6">
@@ -33,7 +33,7 @@ export default function HeaderArtist() {
                 <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
                     <h2
                         class="mb-6 text-3xl font-bold text-neutral-800 dark:text-white dark:text-neutral-200">
-                        Artists
+                        Events
                     </h2>
                     <p class=" dark:text-slate-400 text-gray-400 md:pb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
--- pages/events.js
+++ pages/events.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footers/Footer';
 import EventComponent from '@/components/EventComponent'
+import HeaderEvents from '@/components/HeaderEvents';
 
 export default function() {
 
   return (
     <div>
       <Navbar />
+      <HeaderEvents />
       <EventComponent />
       <Footer />
     </div>
