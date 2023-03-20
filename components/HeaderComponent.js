e3de0953d1fde518f81d30b0dc75ca3b140466b8
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-20 16:05:12 +0100

    Update Harder component

--- components/HeaderComponent.js
+++ components/HeaderComponent.js
@@ -1,6 +1,6 @@
 
 
-export default function HeaderEvents() {
+export default function HeaderEvents(props) {
 
     return (
         <div className="bg-white dark:bg-slate-800 pt-24 px-6">
@@ -30,15 +30,12 @@ export default function HeaderEvents() {
                         </defs>
                     </svg>
                 </div>
-                <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
-                    <h2
-                        class="mb-6 text-3xl font-bold text-neutral-800 dark:text-white dark:text-neutral-200">
-                        Events
+                <div className="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+                    <h2 className="mb-6 text-3xl font-bold text-neutral-800 dark:text-white dark:text-neutral-200">
+                        {props.title ?? ''}
                     </h2>
-                    <p class=" dark:text-slate-400 text-gray-400 md:pb-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
-                        amet numquam iure provident voluptate esse quasi, veritatis totam
-                        voluptas nostrum quisquam eum porro a pariatur veniam.
+                    <p className=" dark:text-slate-400 text-gray-400 md:pb-0">
+                        {props.description ?? ''}
                     </p>
                 </div>
             </div>
