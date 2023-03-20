ed0cd592348b4bb4593b069091a1933d416a208e
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-20 16:39:14 +0100

    Removed useless component from index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import CenterTitle from '@/components/CenterTitle'
 import Description from '@/components/Description'
 import Footer from '@/components/Footers/Footer'
-import GridComponent from '@/components/GridComponent'
 import Navbar from '@/components/Navbar'
 
 
