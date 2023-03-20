03f6a594134f83a9765eb64e45027f7386a97b9a
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-20 16:36:14 +0100

    Update events page

--- pages/events.js
+++ pages/events.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footers/Footer';
 import EventComponent from '@/components/EventComponent'
-import HeaderEvents from '@/components/HeaderEvents';
+import HeaderEvents from '@/components/HeaderComponent';
+import { useTranslation } from 'next-i18next'
+
 
 export default function() {
 
   return (
     <div>
       <Navbar />
-      <HeaderEvents />
+      <HeaderEvents title="Events" description="Our events" />
       <EventComponent />
       <Footer />
     </div>
