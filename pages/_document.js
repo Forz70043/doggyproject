4f9ed28a6da6dca33c3cd8af61be4ee1cb512364
Dantesk <lagalante.p@gmail.com>
2023-03-21 15:31:18 +0100

    [TO-DO] add esplicit dark: in enable? bhooooo

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='dark'>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Grotesk" />
       </Head>
