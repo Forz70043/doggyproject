62a4f6200c9f00c786b0b4cc39e1ec24f14b7195
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-21 18:29:16 +0100

    Added trans on Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import { useTranslation } from 'next-i18next'
+const t = useTranslation();
 
 const navigation = [
   { name: 'Events', href: '/events' },
@@ -11,34 +12,27 @@ const navigation = [
 
 export default function Navbar() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  
     return (
       <div className="bg-white dark:bg-slate-800">
         <header className="absolute inset-x-0 top-0 z-50">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
               <a href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">DoggyProject</span>
-                <img
-                  className="h-8 w-auto"
-                  src='logo_empty.png'
-                  alt=""
-                />
+                <span className="sr-only">Doggy Project</span>
+                <img className="h-8 w-auto" src='logo_empty.png' alt="Logo Doggy Project"/>
               </a>
             </div>
             <div className="flex lg:hidden">
-              <button
-                type="button"
+              <button type="button"
                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(true)}
-              >
+                onClick={() => setMobileMenuOpen(true)}>
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
-              {navigation.map((item) => (
-                <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+              {navigation.map((item, index) => (
+                <a key={index} href={item.href} className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
                   {item.name}
                 </a>
               ))}
@@ -54,18 +48,10 @@ export default function Navbar() {
             <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-slate-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
                 <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">DoggyProject</span>
-                  <img
-                    className="h-8 w-auto"
-                    src="logo_empty.png"
-                    alt=""
-                  />
+                  <span className="sr-only">Doggy Project</span>
+                  <img className="h-8 w-auto" src="logo_empty.png" alt=""/>
                 </a>
-                <button
-                  type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-slate-400"
-                  onClick={() => setMobileMenuOpen(false)}
-                >
+                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-slate-400" onClick={() => setMobileMenuOpen(false)}>
                   <span className="sr-only">Close menu</span>
                   <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
@@ -73,12 +59,9 @@ export default function Navbar() {
               <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="space-y-2 py-6">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 dark:text-slate-400 hover:bg-gray-50"
-                      >
+                    {navigation.map((item, index) => (
+                      <a key={index} href={item.href}
+                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 dark:text-slate-400 hover:bg-gray-50">
                         {item.name}
                       </a>
                     ))}
