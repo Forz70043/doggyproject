9434db66c6ee74ba3f0e4d253f35d519df5042ef
Dantesk <lagalante.p@gmail.com>
2023-03-21 16:04:21 +0100

    change color scheme button and other

--- components/Description.js
+++ components/Description.js
@@ -42,7 +42,7 @@ export default function Description() {
               <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 dark:text-slate-400 ring-1 ring-green-400/100 hover:ring-green-800/20">
                 Announcing our next event of funding.{' '}
                 <a href="/events" className="font-semibold text-yellow-400">
-                  <span className="absolute inset-0 " aria-hidden="true" />
+                  <span className="absolute inset-0" aria-hidden="true" />
                   Read more <span aria-hidden="true">&rarr;</span>
                 </a>
               </div>
@@ -53,7 +53,7 @@ export default function Description() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-green-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
