6fe54fbf8f83023c6db253da48b20d72b93ccc92
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-21 19:19:17 +0100

    Update Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import Header from './Header'
 import Footer from '@/components/Footers/Footer'
+import Navbar from './Navbar';
 
 export default function Layout({children}){
+    console.log("children: ", children.type.name);
     return (
         <>
         <Header title="Doggy Project"/>
+        <Navbar page={children.type.name} />
         <main>
             {children}
         </main>
