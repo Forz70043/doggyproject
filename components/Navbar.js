dcf39599f0ecb0b11e3e63b6efc6653f65bc8857
Dantesk <lagalante.p@gmail.com>
2023-03-21 16:04:07 +0100

    fix navbar: is enabled sticky bar? bhoooooo

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
   
     return (
       <div className="bg-white dark:bg-black">
-        <header className="absolute inset-x-0 top-0 z-50">
+        <header className="sticky top-0 z-50 backdrop-blur-md">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
               <a href="/" className="-m-1.5 p-1.5">
