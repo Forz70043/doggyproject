a55ebce1213f80ee1b380c5753e93b2d1a0a26c5
Dantesk <lagalante.p@gmail.com>
2023-03-22 12:00:58 +0100

    update event components fix text image and block

--- components/EventComponent.js
+++ components/EventComponent.js
@@ -4,19 +4,37 @@ import Image from 'next/image';
 export default function EventComponent() {
     return (
         <>
-        <div className="bg-white dark:bg-black py-24 sm:py-32 flex flex-col items-center">
-            {events.map((elem, index) => {
-                return (
-                    <a key={index} href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-3">
-                        <Image className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/img_avatar.png" alt="" width={100} height={120} />
-                        <div className="flex flex-col justify-between p-4 leading-normal">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{elem.name ?? ''}</h5>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{(elem.description) ? elem.description : (elem.data) ? elem.data : ''}</p>
-                        </div>
-                    </a>
-                )
-            })}
-        </div>
+            <div className="bg-white dark:bg-black py-24 sm:py-32">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col items-center ">
+                        {events.map((elem, index) => {
+                            return (
+                                <div class="flex justify-center mb-5">
+                                    <div
+                                        class="flex flex-col rounded-lg bg-white shadow-lg dark:bg-neutral-700 md:max-w-xl md:flex-row">
+                                        <img
+                                            class="h-80 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                                            src="/img_avatar.png"
+                                            alt="" />
+                                        <div class="flex flex-col justify-start p-6">
+                                            <h5
+                                                class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                                                {elem.name}
+                                            </h5>
+                                            <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                                {elem.description}
+                                            </p>
+                                            <p class="text-xs text-neutral-500 dark:text-neutral-300">
+                                                Last updated 3 mins ago
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
