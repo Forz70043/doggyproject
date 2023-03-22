290ee11da5512dec0c3142cedf278b9485d073a8
Dantesk <lagalante.p@gmail.com>
2023-03-22 11:36:36 +0100

    change color

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -222,7 +222,7 @@ export default function ContactForm(props) {
                                         name="message"
                                         id="message"
                                         rows={4}
-                                        className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 dark:text-slate-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         onChange={handleInput} value={formData.message} required
                                     />
                                 </div>
@@ -250,7 +250,7 @@ export default function ContactForm(props) {
                                 </div>
                                 <Switch.Label className="text-sm leading-6 dark:text-slate-400 text-gray-600">
                                     By selecting this, you agreed to our{' '}
-                                    <a href="#" className="font-semibold text-indigo-600">
+                                    <a href="#" className="font-semibold text-yellow-400">
                                         privacy&nbsp;policy
                                     </a>
                                     .
@@ -260,7 +260,7 @@ export default function ContactForm(props) {
                         <div className="mt-10">
                             <button
                                 type="submit"
-                                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="block w-full rounded-md bg-green-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Let's talk
                             </button>
