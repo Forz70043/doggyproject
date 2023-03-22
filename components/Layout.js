8735550daa3763dc1ed41c4d5a87edd337ace34b
Dantesk <lagalante.p@gmail.com>
2023-03-22 20:47:45 +0100

    format code

--- components/Layout.js
+++ components/Layout.js
@@ -2,16 +2,16 @@ import Header from './Header'
 import Footer from '@/components/Footers/Footer'
 import Navbar from './Navbar';
 
-export default function Layout({children}){
+export default function Layout({ children }) {
     console.log("children: ", children.type.name);
     return (
         <>
-        <Header title="Doggy Project"/>
-        <Navbar page={children.type.name} />
-        <main>
-            {children}
-        </main>
-        <Footer />
+            <Header title="Doggy Project" />
+            <Navbar page={children.type.name} />
+            <main>
+                {children}
+            </main>
+            <Footer />
         </>
     )
 }
