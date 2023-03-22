814cfb7126fbf928829963af8b5a4064ae14d9e5
Forz70043 <alfonso.pisicchio@libero.it>
2023-03-22 17:22:59 +0100

    Update events with img

--- components/EventComponent.js
+++ components/EventComponent.js
@@ -9,24 +9,25 @@ export default function EventComponent() {
                     <div className="flex flex-col items-center ">
                         {events.map((elem, index) => {
                             return (
-                                <div class="flex justify-center mb-5">
+                                <div key={index} className="flex justify-center mb-5">
                                     <div
-                                        class="flex flex-col rounded-lg bg-white shadow-lg dark:bg-neutral-700 md:max-w-xl md:flex-row">
+                                        className="flex flex-col rounded-lg bg-white shadow-lg dark:bg-neutral-700 md:max-w-xl md:flex-row">
                                         <img
-                                            class="h-80 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                                            src="/img_avatar.png"
-                                            alt="" />
-                                        <div class="flex flex-col justify-start p-6">
+                                            className="h-80 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                                            src={elem.src ?? '/img_avatar.png'}
+                                            alt="Event Logo" 
+                                        />
+                                        <div className="flex flex-col justify-start p-6">
                                             <h5
-                                                class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-                                                {elem.name + " - " + elem.date} 
+                                                className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                                                {elem.name + " - " + elem.date}
                                             </h5>
-                                            <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                                                 {elem.description}
                                             </p>
-                                            <p class="text-xs text-neutral-500 dark:text-neutral-300">
+                                            {/* {<p className="text-xs text-neutral-500 dark:text-neutral-300">
                                                 Last updated 3 mins ago
-                                            </p>
+                                            </p>} */}
                                         </div>
                                     </div>
                                 </div>
--- pages/events.js
+++ pages/events.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footers/Footer';
 import EventComponent from '@/components/EventComponent'
 import HeaderEvents from '@/components/HeaderComponent';
 import { useTranslation } from 'next-i18next'
