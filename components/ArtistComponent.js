e34ca3c08896e66eeff30efc78d16f052804802e
Dantesk <lagalante.p@gmail.com>
2023-03-22 13:40:51 +0100

    change card style

--- components/ArtistComponent.js
+++ components/ArtistComponent.js
@@ -7,74 +7,53 @@ export default function ArtistComponent() {
         <>
             <div className="bg-white dark:bg-black py-24 sm:py-32">
                 <div className="container mx-auto px-4">
-                    <div className="flex flex-wrap">
+                    <div className="grid md:grid-cols-3 gap-x-6 lg:gap-x-12">
                         {artists.map((elem, index) => (
-                            <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4 min-w-0 break-words bg-white dark:bg-black w-full mx-12 mb-6 shadow-xl rounded-lg mt-16">
-                                <div className="px-6">
-                                    <div className="flex flex-wrap justify-center">
-                                        <div className="w-full px-4 flex justify-center">
-                                            <div className="relative">
-                                                <img
-                                                    alt="..."
-                                                    src={elem.src}
-                                                    className="shadow-xl h-auto align-middle border-none"
-                                                />
-                                            </div>
-                                        </div>
-                                        {/* <div className="w-full px-4 text-center mt-20">
-                                        <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                                            <div className="mr-4 p-3 text-center">
-                                                <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                                                    22
-                                                </span>
-                                                <span className="text-sm text-blueGray-400">Friends</span>
-                                            </div>
-                                            <div className="mr-4 p-3 text-center">
-                                                <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                                                    10
-                                                </span>
-                                                <span className="text-sm text-blueGray-400">Photos</span>
-                                            </div>
-                                            <div className="lg:mr-4 p-3 text-center">
-                                                <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                                                    89
-                                                </span>
-                                                <span className="text-sm text-blueGray-400">Comments</span>
-                                            </div>
-                                        </div>
-                                    </div> */}
+                            <div className="mb-6 lg:mb-0">
+                                <div className="bg-white dark:bg-gray-800 block rounded-lg shadow-lg">
+                                    <div className="relative overflow-hidden bg-no-repeat bg-cover">
+                                        <img src={elem.src} className="w-full rounded-t-lg" />
+                                        <a href="#!">
+                                            <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"></div>
+                                        </a>
+                                        {/* <svg className="absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"
+                                            style={{ marginLeft: 0, marginBottom: 0 }}>
+                                            <path fill="#fff"
+                                                d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+                                            </path>
+                                        </svg> */}
                                     </div>
-                                    <div className="text-center mt-5">
-                                        <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-                                            {elem.name}
-                                        </h3>
-                                        {/* <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-                                        <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>{" "}
-                                        Los Angeles, California
-                                    </div> */}
-                                        <div className="mb-2 text-blueGray-600 mt-5">
-                                            <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-                                            {"@" + elem.aka}
-                                        </div>
-                                        {/* <div className="mb-2 text-blueGray-600">
-                                        <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-                                        University of Computer Science
-                                    </div> */}
-                                    </div>
-                                    <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-                                        <div className="flex flex-wrap justify-center">
-                                            <div className="w-full lg:w-9/12">
-                                                <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                                                    {elem.description.substr(0, 100)}
-                                                </p>
-                                                <a
-                                                    href="#"
-                                                    className="font-normal text-lightBlue-500"
-                                                    onClick={(e) => e.preventDefault()}
-                                                >
-                                                    Show more
-                                                </a>
-                                            </div>
+                                    <div className="p-6">
+                                        <h5 className="text-lg font-bold mb-4">{elem.name}</h5>
+                                        <p className="text-gray-500 mb-4">{"@" + elem.aka}</p>
+                                        <p className="text-gray-500 mb-4">{elem.description.substr(0, 200)+'...'}</p>
+                                        <ul className="list-inside flex mx-auto justify-center">
+                                            <a href="#!" className="px-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="w-4 h-4 text-blue-600">
+                                                    <path fill="currentColor"
+                                                        d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+                                                </svg>
+                                            </a>
+                                            <a href="#!" className="px-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 text-blue-600">
+                                                    <path fill="currentColor"
+                                                        d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+                                                </svg>
+                                            </a>
+                                            <a href="#!" className="px-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 h-4 text-blue-600">
+                                                    <path fill="currentColor"
+                                                        d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
+                                                </svg>
+                                            </a>
+                                        </ul>
+                                        <div className="text-center mt-10">
+                                            <a
+                                                href="#"
+                                                className="font-normal text-lightBlue-500"
+                                                onClick={(e) => e.preventDefault()}>
+                                                Show more
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -83,6 +62,7 @@ export default function ArtistComponent() {
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
